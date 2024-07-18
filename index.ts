9692e4135e6e531afbd903aa9318bf740c97b06e
unknown <particular6@proton.me>
2024-07-18 19:12:34 +0500

    initializing...

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+})
+
